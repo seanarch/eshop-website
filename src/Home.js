9f3251c49a6cf4e03ad631d42cf91df7e1bec4af
seanarch <zhuqingyun@outlook.com>
2023-06-04 16:06:37 -0600

    Update overall structure

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://m.media-amazon.com/images/I/71-y-2g-q9L._SX3000_.jpg"
+          alt=""
+          className="home__image"
+        />
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
