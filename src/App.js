435ba9d1b330a06fe79f84c2dc2eb5e01ed3ea9d
seanarch <zhuqingyun@outlook.com>
2023-06-04 21:17:16 -0600

    Added checkout router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
-        <Routes>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+
+      <Routes>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -26,12 +27,20 @@ function Header() {
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">
-            <ShoppingBasketIcon fontSize="large" className="itemBasket" />
-          </span>
-          <span className="nav__itemLineTwo nav__basketCount">0</span>
-        </div>
+
+        <Link to="/checkout">
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount"></span>
+          </div>
+        </Link>
+
+        {/* <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount"></span>
+          </div>
+        </Link> */}
       </div>
     </div>
   );
