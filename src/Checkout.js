f1760f412d86a331735fed56c7cb3f0539194568
seanarch <zhuqingyun@outlook.com>
2023-06-05 14:47:33 -0600

    Added checkoutproduct page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 
 export default function Checkout() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="checkout">
+      <div className="chechout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+          <CheckoutProduct />
+        </div>
+      </div>
+
+      <div className="checkout__right"></div>
     </div>
   );
 }
